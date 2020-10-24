ccac9a6d0fe7fc67d70fdd6c591a967ff4a064ec
Oksana Sam <oksana.o.samokhvalova@gmail.com>
2020-10-23 22:51:42 -0400

    configured data test attributes

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { render } from "@testing-library/react";
 import App from "./App";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
@@ -7,55 +6,63 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 // set up enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<App {...props} />);
+};
+
+// const setup = () => shallow(<App />);
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test c1scoL0ve!
+ *
+ */
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 // first test
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
-  expect(appComponent.length).toBe(1);
-  // console.log(wrapper.debug());
-  // expect(wrapper).toBeFalsy();
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  // throw new Error();
-});
-
-test("renders button", () => {
-  // const wrapper = shallow(<App />);
-  // console.log(wrapper.debug());
-  // expect(wrapper).toBeFalsy();
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  // throw new Error();
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 });
 
 test("renders counter display", () => {
-  // const wrapper = shallow(<App />);
-  // console.log(wrapper.debug());
-  // expect(wrapper).toBeFalsy();
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  // throw new Error();
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 });
 
 test("counter starts at 0", () => {
-  // const wrapper = shallow(<App />);
-  // console.log(wrapper.debug());
-  // expect(wrapper).toBeFalsy();
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  // throw new Error();
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0"); // do this first with an integer and show failure!
 });
 
-test("clicking on button increments counter display", () => {
-  // const wrapper = shallow(<App />);
-  // console.log(wrapper.debug());
-  // expect(wrapper).toBeFalsy();
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  // throw new Error();
+describe("Increment", () => {
+  // now we have enough tests to organize by function
+  test("renders increment button", () => {
+    const wrapper = setup();
+    const button = findByTestAttr(wrapper, "increment-button");
+    expect(button.length).toBe(1);
+  });
+
+  test("counter increments when button is clicked", () => {
+    const wrapper = setup();
+
+    // find button and click
+    const button = findByTestAttr(wrapper, "increment-button");
+    button.simulate("click");
+
+    // check the counter
+    const count = findByTestAttr(wrapper, "count").text();
+    expect(count).toBe("1");
+  });
 });
